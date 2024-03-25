29785fabd3278aae8e13634bb52fccec55adea01
chinmaydeolekar <chinmay.deolekar1@gmail.com>
2024-03-25 18:29:27 +0530

    Added expense item component extorted it and imported it in app.js to render that component

---
+++ project-one/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+const ExpenseItem = () => {
+    return (
+        <div>
+            <p>
+                Food - $10<br />
+                Petrol - $20<br />
+                Movie - $10
+            </p>
+        </div>
+    );
+}
+
+export default ExpenseItem;
