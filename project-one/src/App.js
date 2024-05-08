7de71d891a2a1b9d7fa95401db72a3c027f2e582
chinmaydeolekar <chinmay.deolekar1@gmail.com>
2024-05-08 14:33:57 +0530

    used map to give values to components

--- project-one/src/App.js
+++ project-one/src/App.js
@@ -1,15 +1,29 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { id: 1, date: new Date(2023, 7, 15), title: 'Insurance', price: '50', city: 'Bangalore' },
+    { id: 2, date : new Date(2023, 8, 16), title: 'Book', price: '20', city: 'Delhi' },
+    { id: 3, date: new Date(2023, 9, 17), title: 'Pen', price: '10', city: 'Hyderabad' },
+    { id: 4, date: new Date(2023, 10, 18), title: 'Laptop', price: '200', city: 'Mumbai' }
+  ]
   return (
     <div>
       <h1>Let's get Started</h1>
-      <ExpenseItem date = {new Date(2023, 7, 15)} title = 'Insurance' price = '50' city = 'Bangalore'></ExpenseItem>
-      <ExpenseItem date = {new Date(2023, 8, 16)} title = 'Book' price = '20' city = 'Delhi'></ExpenseItem>
-      <ExpenseItem date = {new Date(2023, 9, 17)} title = 'Pen' price = '10' city = 'Hyderabad'></ExpenseItem>
-      <ExpenseItem date = {new Date(2023, 10, 18)} title = 'Laptop' price = '200' city = 'Mumbai'></ExpenseItem>
+      {
+        expenses.map((expense, index) => {
+          return (
+            <ExpenseItem
+              date={expense.date}
+              title={expense.title}
+              price={expense.price}
+              city={expense.city}
+            ></ExpenseItem>
+          )
+        })
+      }
     </div>
   );
-}
+} 
 
 export default App;
