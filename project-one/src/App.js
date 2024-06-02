a14d30e1d6ece152f9160e70fa92edab47283af9
chinmaydeolekar <chinmay.deolekar1@gmail.com>
2024-06-02 16:02:47 +0530

    changes

--- project-one/src/App.js
+++ project-one/src/App.js
@@ -4,9 +4,9 @@ import Expense from "./components/Expenses"
 function App() {
   const expenses = [
     { id: 1, date: new Date(2023, 7, 15), title: 'Insurance', price: '100', city: 'Bangalore' },
-    { id: 2, date : new Date(2023, 7, 25), title: 'Book', price: '10', city: 'Delhi' },
-    { id: 3, date: new Date(2023, 7, 11), title: 'Pen', price: '1', city: 'Hyderabad' },
-    { id: 4, date: new Date(2023, 7, 14), title: 'Laptop', price: '200', city: 'Mumbai' }
+    { id: 2, date : new Date(2023, 3, 25), title: 'Book', price: '10', city: 'Delhi' },
+    { id: 3, date: new Date(2023, 10, 11), title: 'Pen', price: '1', city: 'Hyderabad' },
+    { id: 4, date: new Date(2023, 1, 14), title: 'Laptop', price: '200', city: 'Mumbai' }
   ]
   return (
     <div>
