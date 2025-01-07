ecbad96b06d8104c8b120db829a22a4105665d42
chinmay_deolekar <nodejs.1@hanooman.ai>
2025-01-07 15:28:42 +0530

    added the conditional rendering

--- project-one/src/components/NewExpense/ExpenseForm.js
+++ project-one/src/components/NewExpense/ExpenseForm.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
+  const [isExpanded, setIsExpanded] = useState(false)
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -56,6 +57,7 @@ const ExpenseForm = (props) => {
   };
 
   const submmitChangeHandler = (event) => {
+    setIsExpanded(!isExpanded);
     event.preventDefault();
     const infoToUpdate = {
       title: enteredTitle,
@@ -71,30 +73,34 @@ const ExpenseForm = (props) => {
 
   return (
     <form onSubmit={submmitChangeHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label htmlFor="title">Title</label>
-          <input type="text" value={enteredTitle} id="title" onChange={titleChangeHandler} />
-        </div>
-        <div className="new-expense__control">
-          <label htmlFor="price">Price</label>
-          <input type="number" value={enteredAmount} id="amount" onChange={amountChangeHandler} />
-        </div>
-        <div className="new-expense__control">
-          <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            value={enteredDate}
-            id="date"
-            min="2023-01-01"
-            max="2025-12-31"
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
+      { isExpanded && ( <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label htmlFor="title">Title</label>
+            <input type="text" value={enteredTitle} id="title" onChange={titleChangeHandler} />
+          </div>
+          <div className="new-expense__control">
+            <label htmlFor="price">Price</label>
+            <input type="number" value={enteredAmount} id="amount" onChange={amountChangeHandler} />
+          </div>
+          <div className="new-expense__control">
+            <label htmlFor="date">Date</label>
+            <input
+              type="date"
+              value={enteredDate}
+              id="date"
+              min="2023-01-01"
+              max="2025-12-31"
+              onChange={dateChangeHandler}
+            />
+          </div>
+        </div> )
+      }
+      {
+        !isExpanded ? ( <button type="submit" onClick={submmitChangeHandler}>Add Expense</button> ) : ( <div className="new-expense__actions">
+          <button type="submit">Cancel</button>
+          <button type="submit" onSubmit={submmitChangeHandler}>Add Expense</button>
+        </div> )
+      }
     </form>
   );
 };
